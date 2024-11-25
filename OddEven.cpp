15fd223d44cbc5724c2955d1a4f34480d3cb9a47
Maritsa-AW <maritsaaikowibisono@mail.ugm.ac.id>
2024-11-25 14:28:59 +0700

    Add files via upload

---
+++ OddEven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double n, Odd, Even, increment;
+	cout<<"Enter the list of number: "<<endl;
+	cin>>n;
+	if {
+	Even = (n%2 == 0);
+	Odd = (n%1 ==0);
+}
+	else{
+	increment odd counter;
+	}
+	return 0;
+}
+
