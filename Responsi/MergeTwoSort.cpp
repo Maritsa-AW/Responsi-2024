8d5129d3e3c3097184c5da448dc23c9e1d9e465b
Maritsa-AW <maritsaaikowibisono@mail.ugm.ac.id>
2024-11-25 14:28:08 +0700

    Add files via upload

---
+++ Responsi/MergeTwoSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int n, A, B;
+	long long Merge;
+	
+	A = (1,3,4,6,5) , B = (1,2,7,8,9,3)
+	n = (i++, n>2)
+	
+	cout<<"The merge of the two sorted list is "<<Merge<<endl;
+}
